a2cbede639f83c003499a03223da44c28cf632cf
Krimm <Krimm@DESKTOP-2Q5K4AH>
2016-12-25 11:47:31 +0100

    start of EmplyeeREST

---
+++ EmployeeRESTService/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace EmployeeRESTService
+{
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public int PhoneNumber { get; set; }
+        [DataMember]
+        public string Position { get; set; }
+    }
+}
